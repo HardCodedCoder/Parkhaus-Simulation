cb8b56cbeef7a60633b0877a395726276b5a80ff
Linda <2210859017@fh-burgenland.at>
2024-01-03 16:33:23 +0100

    feat: SimulationServiceTest

--- src/Test/java/SimulationServiceTest.java
+++ src/Test/java/SimulationServiceTest.java
@@ -1,5 +1,46 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.lecture.SimulationService;
+import org.lecture.interfaces.IOHandler;
+import org.lecture.interfaces.Service;
 import org.lecture.view.ConsoleIOHandler;
 
+import java.io.*;
+
 public class SimulationServiceTest {
-    private ConsoleIOHandler ioHandler;
+
+    /**
+     * Test if the SimulationService works as it should
+     */
+    @Test
+    public void testSimulationService() {
+        InputStream sysIn = System.in;
+        OutputStream sysOut = System.out;
+        String input = "3\n3,20,ja\n1\n4";
+        ByteArrayInputStream is = new ByteArrayInputStream(input.getBytes());
+        System.setIn(is);
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bo, true));
+
+        IOHandler ioHandler = new ConsoleIOHandler();
+        SimulationService.setIOHandler(ioHandler);
+        Service applicationService = new SimulationService(ioHandler);
+        applicationService.run();
+
+        String output = bo.toString();
+        output = output.replace("Bitte wählen Sie eine Option: ", "");
+        String[] lines = output.split("\n");
+        int actual = 0;
+        for(String line : lines){
+            if(line.startsWith("[")){
+                actual++;
+            }
+        }
+
+        int expected = 9;
+        System.setIn(sysIn);
+        System.setOut((PrintStream) sysOut);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
